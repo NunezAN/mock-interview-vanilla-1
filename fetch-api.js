62b18044b8154557608c3cd0551e7451855a1b8b
NunezAN <alex101nunez@gmail.com>
2023-03-02 20:45:24 -0600

    fetching data and displaying with map

--- fetch-api.js
+++ fetch-api.js
@@ -1,12 +1,20 @@
 import axios from "axios";
 
+const fetchedDatas = [];
 const fetchData = async () => {
   const fetchedData = await axios.get("https://www.boredapi.com/api/activity/");
-  displayData(fetchedData);
+  fetchedDatas.push(fetchedData.data.activity);
+  console.log(fetchedDatas);
+  displayData(fetchedDatas);
 };
 
 fetchData();
 
-const displayData = (fetchedData) => {
-  document.getElementById("api-data").innerHTML = fetchedData.data.activity;
+const displayData = (fetchedDatas) => {
+  document.getElementById("api-data").innerHTML = fetchedDatas
+    .map((data) => `<div>${data}</div>`)
+    .join("");
 };
+document
+  .querySelector("#fetch-button")
+  .addEventListener("click", () => fetchData());
